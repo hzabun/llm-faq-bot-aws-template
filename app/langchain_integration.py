f48b3b715187d57443718ced1500c1e826c2a31a
Halil <halil.zabun@icloud.com>
2025-07-16 21:55:28 +0200

    fix(app): use recommended langchain huggingface package

--- app/langchain_integration.py
+++ app/langchain_integration.py
@@ -6,7 +6,7 @@ from langchain.schema import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader, UnstructuredMarkdownLoader
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 logging.basicConfig(level=logging.INFO)
 
